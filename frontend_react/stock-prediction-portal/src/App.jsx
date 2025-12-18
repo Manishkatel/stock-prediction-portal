62a2b9179d0c1e9e36514abe0a792fa1173d36bb
Manishkatel <manishkatel2020@gmail.com>
2025-12-17 19:10:48 -0600

    created routes for the pages

--- frontend_react/stock-prediction-portal/src/App.jsx
+++ frontend_react/stock-prediction-portal/src/App.jsx
@@ -1,15 +1,25 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
-import Home from './components/Home'  // Import the Main component
+import Home from './components/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Register from "./components/Register"
 
 function App() {
+
   return (
     <>
+
+    <BrowserRouter>
     <Header />
-    <Home />  
-    <Footer />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Register />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+
+   </>
   )
 }
 
-export default App
+export default App 
